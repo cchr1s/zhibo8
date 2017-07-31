253b1ca52a5e0444dd43d98c445d6614ae16cdaa
cc <maodouzhajiang@gmail.com>
2017-07-31 18:29:34 +0800

    amend app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const config = require('config-lite')
 const compression = require('compression')
 const app = express()
 
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
